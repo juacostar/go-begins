b08bbf2b41bbd89d081fdc4aeb567daa96da6cbf
Juan Camilo Acosta Rojas <juacostar@unal.edu.co>
2023-06-04 01:40:20 -0500

    calculator project

--- calculator/calculator.go
+++ calculator/calculator.go
@@ -8,26 +8,60 @@ import (
 	"strconv"
 )
 
-// delete aditional settings in settings extension go extension
+type calc struct{} // for create stricutures
 
-func main() {
+func (calc) operate(value string, operator string) int { // receiver function, call gets this method for his structure- Like method of a class
 
-	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Scan()
+	cleanInput := strings.Split(value, operator)
+	operator1 := parseTonInt(cleanInput[0])
+	operator2 := parseTonInt(cleanInput[1])
 
-	operation := scanner.Text()
-	values := strings.Split(operation, "+")
-	fmt.Println(values)
-	fmt.Println(operation)
-	fmt.Println(values)
+	switch operator {
+	
+	case "+":
+		fmt.Println(operator1 + operator2)
+		return operator1 + operator2
+	case "-":
+		fmt.Println(operator1 - operator2)
+		return operator1 - operator2
+	case "*":
+		fmt.Println(operator1 * operator2)
+		return operator1 * operator2
+	case "/":
+		fmt.Println(operator1 / operator2)
+		return operator1 / operator2
+	default:
+		fmt.Println("Operator not supported")
+		return 0
+	}
+}
 
-	operator1, err1 := strconv.Atoi(values[0]) // _ for catching errors 
+func parseTonInt(enter string) int {
+	operator1, err1 := strconv.Atoi(enter) // _ for catching errors 
 	if err1 != nil {// nil<=>null
 		fmt.Println(err1)
 	}
 
-	operator2, _ := strconv.Atoi(values[1])
+	return operator1
+}
 
-	fmt.Println(operator1 + operator2)
+func readInput() string {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+
+	operation := scanner.Text()
+
+	return operation
+}
+
+// delete aditional settings in settings extension go extension
+
+func main() {
+	
+	input := readInput()
+	operator:= readInput()
+
+	calculator := calc{}
+	calculator.operate(input, operator)
 
 }
